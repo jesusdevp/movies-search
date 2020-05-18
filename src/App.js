180f9d1ccc06125ee452387415d06483847445ba
Jesus Ramirez <jesusdevelp@gmail.com>
2020-05-18 15:05:25 -0500

    capturing search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Components/Search";
 
 function App() {
+  const [state, setstate] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=2130b0bb";
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setstate((prevState) => {
+      return { ...prevState, s: s };
+    });
+
+    console.log(s);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movies Search</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
